c929799595f9090356769150feb0b7bde1b4c4f3
soura-vvv <souravpande39@gmail.com>
2024-03-21 18:32:04 -0400

    WORKINGGGG

--- infer2.py
+++ infer2.py
@@ -176,6 +176,7 @@ class SEBrain(sb.Brain):
             test_loader_kwargs["ckpt_prefix"] = None
             test_set = self.make_dataloader(
                 test_set, Stage.TEST, **test_loader_kwargs
+                )
         self.on_evaluate_start(max_key=max_key, min_key=min_key)
         self.modules.eval()
         with torch.no_grad():
