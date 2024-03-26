6ea44d5b8926f732927ef745832342c3e5ac342b
soura-vvv <souravpande39@gmail.com>
2024-03-26 17:29:05 -0400

    data io attempt

--- zoroTrain.py
+++ zoroTrain.py
@@ -48,7 +48,7 @@ class SEBrain(sb.Brain):
         # We first move the batch to the appropriate device, and
         # compute the features necessary for masking.
         print("Batchbatchbatcb-------------------------")
-        print(batch.clean_sig)
+        print(batch.coordinates)
         exit()
         batch = batch.to(self.device)
         self.clean_wavs, self.lens = batch.clean_sig
