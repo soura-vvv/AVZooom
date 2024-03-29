e38599ab5be654efffda6db921c58f011429cdb0
soura-vvv <souravpande39@gmail.com>
2024-03-28 22:14:04 -0400

    trying new dataio

--- zoroTrain.py
+++ zoroTrain.py
@@ -322,7 +322,7 @@ def dataio_prep(hparams):
             json_path=data_info[dataset],
             replacements={"data_root": hparams["data_folder"]},
             dynamic_items=[audio_pipeline,audio_pipeline2],
-            output_keys=["id", "clean_sig","noisy_wav","coordinates"],
+            output_keys=["id", "clean_sig","noisy_sig","coordinates"],
         ).filtered_sorted(sort_key="length")
     return datasets
 
