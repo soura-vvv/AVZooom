b827eb47c2f007fb57b9d6567d5cf6f79e2b9396
soura-vvv <souravpande39@gmail.com>
2024-04-03 20:55:38 -0400

    Json Paths

--- zoroTrain.py
+++ zoroTrain.py
@@ -318,6 +318,7 @@ def dataio_prep(hparams):
     hparams["dataloader_options"]["shuffle"] = False
     for dataset in data_info:
         print("LetsGOOOO")
+        print(data_info[dataset])
         datasets[dataset] = sb.dataio.dataset.DynamicItemDataset.from_json(
             json_path=data_info[dataset],
             replacements={"data_root": hparams["data_folder"]},
