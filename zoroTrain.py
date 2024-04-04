a539d384302a5e87a29d51f1d193b924e0e8d563
soura-vvv <souravpande39@gmail.com>
2024-04-03 23:32:08 -0400

    JSON MODSSS

--- zoroTrain.py
+++ zoroTrain.py
@@ -381,7 +381,7 @@ if __name__ == "__main__":
     # stopped at any point, and will be resumed on next call.
     print(hparams["model"])
     #print(hparams["skip_prep"])
-    print(datasets["train"][0])
+    print(datasets["valid"][0])
     
     exit()
     se_brain.fit(
